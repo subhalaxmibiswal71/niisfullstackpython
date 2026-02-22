64a17593a7e9d18f493acdb4f6d59dfc0ab23656
subhalaxmi <subhalaxmibiswal409@gmail.com>
2026-02-22 23:41:33 +0530

    test

---
+++ pythonbasic/cin.py
@@ -0,0 +1,10 @@
+print("enter principal")
+P=float(input())
+print("enter rate")
+r=float(input())
+print("enter time")
+t=float(input())
+print("enter number of times compounded")
+n=int(input())
+CI=P*(1+r/n)**(n*t)-P
+print("compound interest=",CI)
