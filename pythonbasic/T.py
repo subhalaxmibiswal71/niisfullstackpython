828a0390dc5d04e5fc7379af2b7df3efdc833e87
subhalaxmi <subhalaxmibiswal409@gmail.com>
2026-02-22 19:09:16 +0530

    test

---
+++ pythonbasic/T.py
@@ -0,0 +1,6 @@
+print("enter base")
+b=float(input())
+print("enter height")
+h=float(input())
+area=0.5*b*h
+print("area=" ,area)
---
+++ pythonbasic/sphere.py
@@ -0,0 +1,4 @@
+print("enter radius")
+r=float(input())
+area=4*3.14*r*r
+print("surface area=" ,area)
