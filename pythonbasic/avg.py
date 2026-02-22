93b8e5f6c721d371c188148abd179d8a233477ba
subhalaxmi <subhalaxmibiswal409@gmail.com>
2026-02-22 19:21:30 +0530

    test

---
+++ pythonbasic/avg.py
@@ -0,0 +1,8 @@
+print("enter first number")
+a=int(input())
+print("enter second number")
+b=int(input())
+print("enter third number")
+c=int(input())
+avrage=(a+b+c)/3
+print("average=" ,avrage)
---
+++ pythonbasic/sc.py
@@ -0,0 +1,6 @@
+print("enter a number")
+n=int(input())
+sq=n*2
+cb=n**3
+print("square=" ,sq)
+print("cube=" ,cb)
