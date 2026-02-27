e492474c5c00809c12bd8d278750c0159b16bc0d
subhalaxmi <subhalaxmibiswal409@gmail.com>
2026-02-27 10:35:56 +0530

    test

---
+++ pythonbasic/function/F2.py
@@ -0,0 +1,5 @@
+def check_odd(num):
+	if num%2!=0:
+		print("enter odd number")
+number=int(input())
+check_odd(number)
---
+++ pythonbasic/function/F3.py
@@ -0,0 +1,10 @@
+def sical():
+	print("enter principal")
+	P=float(input())
+	print("enter rate")
+	R=float(input())
+	print("enter time")
+	T=float(input())
+	SI=(P*R*T)/100
+	print("simple intrest=" ,SI)
+sical()
